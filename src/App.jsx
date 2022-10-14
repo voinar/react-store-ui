29100c34acb60bad13fb4339c4d2213044131142
voinar <nifeprty@gmail.com>
2022-10-14 11:43:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import AppContext, { AppProvider } from './context/AppContext';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import axios from 'axios';
+import { BrowserRouter } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import CategoryRoutes from './components/CategoryRoutes';
-import ProductListing from './pages/storefront/ProductListing';
-import ProductDescription from './pages/storefront/ProductDescription';
-import Cart from './pages/storefront/Cart';
-import LoadingSpinner from './components/LoadingSpinner';
-
-import { serverUrl } from './graphql/Queries';
 
 import './styles/style.css';
 
